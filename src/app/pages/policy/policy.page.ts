8cd427a075ac5deabfbacb2a0943f98de0fae73f
ahmad <ahmad.94192@gmail.com>
2024-02-25 00:57:15 +0700

    STG-002(Kendaran)Persiapan Set Sata Additional Risk
    Signed-off-by: ahmad <ahmad.94192@gmail.com>

--- src/app/pages/policy/policy.page.ts
+++ src/app/pages/policy/policy.page.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-policy',
   templateUrl: './policy.page.html',
   styleUrls: ['./policy.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PolicyPage implements OnInit {
   model1: any = '';
   model2: any = '';
+
+  constructor(
+    private cdRef: ChangeDetectorRef
+  ) {
+  }
+
   ngOnInit() {
+
   }
 }

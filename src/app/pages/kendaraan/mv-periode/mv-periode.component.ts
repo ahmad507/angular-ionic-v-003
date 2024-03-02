8491320ff7a3c7cef92edf16215cfb00fa15d432
ahmad <ahmad.94192@gmail.com>
2024-03-02 19:26:07 +0700

    STG-002(Kendaran)Persiapan Set Sata Additional Risk
    Signed-off-by: ahmad <ahmad.94192@gmail.com>

---
+++ src/app/pages/kendaraan/mv-periode/mv-periode.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-mv-periode',
+  templateUrl: './mv-periode.component.html',
+  styleUrls: ['./mv-periode.component.scss'],
+})
+export class MvPeriodeComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
